3044b009236dc4ac41085d7196ce924b2ba0266d
Савельев Сергей Алексеевич <s.savelyev@nactacredit.com>
2024-04-12 14:56:32 +0300

    453

--- app.js
+++ app.js
@@ -16,6 +16,25 @@ btn1.addEventListener("click", function(){
 });
 
 
+let btn_setup = document.getElementById("test_setup");
+
+btn_setup.addEventListener('click', function(){
+    if (isset($_POST['test_fio'])) {
+        console.log('test_fio');
+    } 
+    if (isset($_POST['test_last_name'])) {
+        console.log('test_last_name');
+    } 
+    if (isset($_POST['test_first_name'])) {
+        console.log('test_first_name');
+    } 
+    if (isset($_POST['test_father_name'])) {
+        console.log('test_father_name');
+    } 
+    if (isset($_POST['test_birthday'])) {
+        console.log('test_birthday');
+    } 
+})
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){

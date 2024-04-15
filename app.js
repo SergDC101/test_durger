10d12c0ee570539671292b7ab27a7c7e1c16ed41
Савельев Сергей Алексеевич <s.savelyev@nactacredit.com>
2024-04-15 09:47:01 +0300

    2552

--- app.js
+++ app.js
@@ -7,34 +7,28 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
-let btn1 = document.getElementById("test_limits");
+let btn_limits = document.getElementById("test_limits");
+let btn_anketa = document.getElementById("test_anketa");
+let btn_lk = document.getElementById("test_lk");
 
-
-btn1.addEventListener("click", function(){
+btn_limits.addEventListener("click", function(){
     document.getElementById("btn_block").style.display = "none";
-    document.getElementById("form_limit").style.display = "block";
+    document.getElementById("btn_block_limits").style.display = "block";
 });
 
 
-let btn_setup = document.getElementById("test_setup");
+btn_anketa.addEventListener("click", function(){
+    document.getElementById("btn_block").style.display = "none";
+    document.getElementById("btn_block_anketa").style.display = "block";
+});
 
-btn_setup.addEventListener('click', function(){
-    if (isset($_POST['test_fio'])) {
-        console.log('test_fio');
-    } 
-    if (isset($_POST['test_last_name'])) {
-        console.log('test_last_name');
-    } 
-    if (isset($_POST['test_first_name'])) {
-        console.log('test_first_name');
-    } 
-    if (isset($_POST['test_father_name'])) {
-        console.log('test_father_name');
-    } 
-    if (isset($_POST['test_birthday'])) {
-        console.log('test_birthday');
-    } 
-})
+btn_lk.addEventListener("click", function(){
+    document.getElementById("btn_block").style.display = "none";
+    document.getElementById("btn_block_lk").style.display = "block";
+});
+
+
+
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){

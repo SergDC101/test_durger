8c6172966869cbb681981e79102aa5da3bd3074f
Савельев Сергей Алексеевич <s.savelyev@nactacredit.com>
2024-04-16 10:18:47 +0300

    234

--- app.js
+++ app.js
@@ -10,21 +10,34 @@ let item = "";
 let btn_limits = document.getElementById("test_limits");
 let btn_anketa = document.getElementById("test_anketa");
 let btn_lk = document.getElementById("test_lk");
+let btn_back = document.getElementById("main_menu");
 
 btn_limits.addEventListener("click", function(){
     document.getElementById("btn_block").style.display = "none";
     document.getElementById("btn_block_limits").style.display = "block";
+    document.getElementById("main_menu").style.display = "block";
 });
 
 
 btn_anketa.addEventListener("click", function(){
     document.getElementById("btn_block").style.display = "none";
     document.getElementById("btn_block_anketa").style.display = "block";
+    document.getElementById("main_menu").style.display = "block";
 });
 
 btn_lk.addEventListener("click", function(){
     document.getElementById("btn_block").style.display = "none";
     document.getElementById("btn_block_lk").style.display = "block";
+    document.getElementById("main_menu").style.display = "block";
+});
+
+
+btn_back.addEventListener("click", function(){
+    document.getElementById("main_menu").style.display = "none";
+    document.getElementById("btn_block_limits").style.display = "none";
+    document.getElementById("btn_block_anketa").style.display = "none";
+    document.getElementById("btn_block_lk").style.display = "none";
+    document.getElementById("btn_block").style.display = "block";
 });
 
 
